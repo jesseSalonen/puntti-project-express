a7c49e60f44f78f552ab89868b0a7281f4df315f
jesseSalonen <jesse.salonen@tuni.fi>
2023-03-21 21:19:33 +0200

    User registration

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const router = express.Router();
 const {
   getExercises,
   addExercise,
   updateExercise,
   deleteExercise,
 } = require("../controllers/ExerciseController");
-const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/UserController");
 
 // Exercises
 router.route("/exercises").get(getExercises).post(addExercise);
 router.route("/exercises/:id").put(updateExercise).delete(deleteExercise);
+
+// Users
+router.post("/users", registerUser);
+router.post("/users/login", loginUser);
+router.get("/users/me", getMe);
 
 module.exports = router;
