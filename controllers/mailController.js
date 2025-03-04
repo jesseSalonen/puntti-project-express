4f7d3cf1db2bae1b99d2176bc0f6dc8bb077c31a
jesseSalonen <jesse.salonen@tuni.fi>
2025-03-04 22:23:35 +0200

    LOKITUS

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -5,6 +5,7 @@ const axios = require("axios");
 
 getZohoAccessToken = asyncHandler(async () => {
     try {
+        console.log(process.env.ZOHO_MAIL_ACCOUNT);
         const response = await axios.post("https://accounts.zoho.eu/oauth/v2/token", null, {
             params: {
                 refresh_token: process.env.ZOHO_REFRESH_TOKEN,
