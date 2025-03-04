118573250262a95ccd5ce86c2fc669880a9b6f73
jesseSalonen <jesse.salonen@tuni.fi>
2025-03-04 22:02:44 +0200

    mailin lähetys

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -3,19 +3,16 @@ const logger = require("../logger");
 const { StatusCodes } = require("http-status-codes");
 const axios = require("axios");
 
-const ZOHO_MAIL_ACCOUNT = process.env.ZOHO_MAIL_ACCOUNT;
-
 getZohoAccessToken = asyncHandler(async () => {
     try {
-        const response = await axios.post("https://accounts.zoho.com/oauth/v2/token", null, {
+        const response = await axios.post("https://accounts.zoho.eu/oauth/v2/token", null, {
             params: {
                 refresh_token: process.env.ZOHO_REFRESH_TOKEN,
                 client_id: process.env.ZOHO_CLIENT_ID,
-                client_secret: process.env.ZOHO_REFRESH_TOKEN,
+                client_secret: process.env.ZOHO_CLIENT_SECRET,
                 grant_type: "refresh_token"
             }
         });
-        console.log(response.data);
         return response.data.access_token;
     } catch (error) {
         logger.error(error);
@@ -24,7 +21,7 @@ getZohoAccessToken = asyncHandler(async () => {
 });
 
 const sendEmail = asyncHandler(async (req, res) => {
-    const { name, email, message } = req.body;
+    const { name, email, message, formId } = req.body;
 
     const accessToken = await getZohoAccessToken();
     if (!accessToken) {
@@ -32,16 +29,31 @@ const sendEmail = asyncHandler(async (req, res) => {
         throw new Error("Could not get Zoho Access Token");
     }
 
+    let subject;
+    switch (formId) {
+        case "consult-form":
+            subject = "Uusi konsultaatiopyyntö";
+            break;
+        case "general-form":
+            subject = "Uusi yhteydenotto";
+            break;
+        case "bike-form":
+            subject = "Uusi pyörävuokrauspyyntö";
+            break;
+        default:
+            res.status(StatusCodes.BAD_REQUEST);
+            throw new Error("Invalid formId");
+    }
+
     try {
         const emailData = {
             fromAddress: process.env.ZOHO_MAIL_ACCOUNT,
             toAddress: process.env.ZOHO_MAIL_ACCOUNT,
-            subject: `Uusi yhteydenotto: ${name}`,
-            content: `Nimi: ${name}\nSähköposti: ${email}\n\nViesti:\n${message}`,
-            contentType: "text/plain"
+            subject: `${subject}: ${name}`,
+            content: `Nimi: ${name}<br>Sähköposti: ${email}<br><br>${message}`
         };
 
-        const response = await axios.post("https://mail.zoho.com/api/accounts/me/messages", emailData, {
+        await axios.post("https://mail.zoho.eu/api/accounts/6603764000000002002/messages", emailData, {
             headers: {
                 Authorization: `Zoho-oauthtoken ${accessToken}`,
                 "Content-Type": "application/json"
